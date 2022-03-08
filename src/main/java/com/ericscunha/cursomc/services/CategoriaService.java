2fe5ab26f20b398cc9da6e64c4483e855e17c8f5
Eric Cunha <ericscunha@hotmail.com>
2022-03-08 16:53:55 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.ericscunha.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericscunha.cursomc.domain.Categoria;
+import com.ericscunha.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria buscar(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElse(null);
+		
+	}
+
+}
