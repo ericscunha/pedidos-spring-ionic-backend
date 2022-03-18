edc047531500e320e7ead85edd7b1da4d9d43d8c
Eric Cunha <ericscunha@hotmail.com>
2022-03-18 09:52:51 -0300

    Endpoint /clientes/{id} disponivel

---
+++ src/main/java/com/ericscunha/cursomc/services/ClienteService.java
@@ -0,0 +1,26 @@
+package com.ericscunha.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericscunha.cursomc.domain.Cliente;
+import com.ericscunha.cursomc.repositories.ClienteRepository;
+import com.ericscunha.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class ClienteService {
+	
+	@Autowired
+	private ClienteRepository repo;
+	
+	public Cliente buscar(Integer id) {
+		Optional<Cliente> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " +  id
+				+ ", Tipo: " + Cliente.class.getName()));
+		
+	}
+
+}
