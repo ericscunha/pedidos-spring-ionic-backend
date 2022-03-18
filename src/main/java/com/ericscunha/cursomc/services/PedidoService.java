012808da6fd335bf22d8bcefadc38211795a46b0
Eric Cunha <ericscunha@hotmail.com>
2022-03-18 15:06:52 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/ericscunha/cursomc/services/PedidoService.java
@@ -0,0 +1,26 @@
+package com.ericscunha.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ericscunha.cursomc.domain.Pedido;
+import com.ericscunha.cursomc.repositories.PedidoRepository;
+import com.ericscunha.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " +  id
+				+ ", Tipo: " + Pedido.class.getName()));
+		
+	}
+
+}
