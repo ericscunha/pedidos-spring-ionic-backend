61b7bc4d2294c971c0e89ef6c2de31bf17cb8d8d
Eric Cunha <ericscunha@hotmail.com>
2022-03-25 19:39:17 -0300

    Atualizando Categoria com PUT, outros pequenos ajustes

--- src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
+++ src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
@@ -15,7 +15,7 @@ public class CategoriaService {
 	@Autowired
 	private CategoriaRepository repo;
 	
-	public Categoria buscar(Integer id) {
+	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " +  id
@@ -28,5 +28,10 @@ public class CategoriaService {
 		return repo.save(obj);
 	}
 	
+	
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
 
 }
