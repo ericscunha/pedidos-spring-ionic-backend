354f3a3eff8725c20a7c948927c64ff10c63099e
Eric Cunha <ericscunha@hotmail.com>
2022-03-25 20:16:32 -0300

    Listando todas Categorias com DTO

--- src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
+++ src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.ericscunha.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,8 +44,11 @@ public class CategoriaService {
 		}
 		catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos.");
-		}
-		
+		}	
+	}
+	
+	public List<Categoria> findAll() {
+		return repo.findAll();
 	}
 
 }
