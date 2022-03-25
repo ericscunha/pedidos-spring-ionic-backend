b222d004d7d41a284b3e1744efb90969e11462d5
Eric Cunha <ericscunha@hotmail.com>
2022-03-25 18:31:37 -0300

    inserindo uma nova Categoria com o POST

--- src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
+++ src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
@@ -22,5 +22,11 @@ public class CategoriaService {
 				+ ", Tipo: " + Categoria.class.getName()));
 		
 	}
+	
+	public Categoria insert(Categoria obj) {
+		obj.setId(null);
+		return repo.save(obj);
+	}
+	
 
 }
