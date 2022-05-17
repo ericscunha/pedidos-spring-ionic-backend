5976a3bd197df62dbf8ed2fec614433a131aa183
Eric Cunha <ericscunha@hotmail.com>
2022-05-17 17:28:57 -0300

    Paginacao com parametros opcionais na requisicao

--- src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
+++ src/main/java/com/ericscunha/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.ericscunha.cursomc.domain.Categoria;
@@ -27,7 +30,7 @@ public class CategoriaService {
 	}
 	
 	public Categoria insert(Categoria obj) {
-		obj.setId(null);
+		obj.setId(null); // Identifica que tem que ter um ID nulo.
 		return repo.save(obj);
 	}
 	
@@ -49,6 +52,11 @@ public class CategoriaService {
 	
 	public List<Categoria> findAll() {
 		return repo.findAll();
+	}
+	
+	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
 	}
 
 }
